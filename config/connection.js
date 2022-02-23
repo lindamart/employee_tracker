b46b47c6458902ebc5cfd9a66efd1a247d5c6483
Linda Martinez <lindanmjw@gmail.com>
2022-02-23 15:15:25 -0500

    changing to uncommon number

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3306,
+  port: 3301,
   user: 'root',
   password: process.env.MYSQL_PASSWORD,
   database: 'employees'
